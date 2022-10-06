737e68ac55b74985d1127f60299191c5c91e999c
lindajohnson7511 <104142506+lindajohnson7511@users.noreply.github.com>
2022-10-06 14:11:33 +0500

    Update prepare_and_run_rcnn.sh

--- prepare_and_run_rcnn.sh
+++ prepare_and_run_rcnn.sh
@@ -9,7 +9,6 @@ ROOT=~/.torch/datasets
 mkdir -p $ROOT/coco/annotations
 echo "$ROOT"
 
-for anno in instances_val2017_100 \
   person_keypoints_val2017_100 ; do
 
   dest=$ROOT/coco/annotations/$anno.json
@@ -34,7 +33,6 @@ IMG_PATH=$ROOT/coco/val2017
 
 MODEL_PATH=~/.torch/model
 mkdir -p $MODEL_PATH
-MODEL_NAME=mask_rcnn_R_50_FPN
 
 wget $BASE/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl -O tmp/pt_$MODEL_NAME.pkl
 
@@ -51,7 +49,6 @@ python3 examples/02_detectron2/tools/convert_pt2ait.py  \
   --model-name $MODEL_NAME
 
 python3 examples/02_detectron2/demo.py \
-  --weight tmp/ait_$MODEL_NAME.pt \
   --config $cfg \
   --batch 1 --input "$IMG_PATH/*.jpg" \
   --confidence-threshold 0.5 \
